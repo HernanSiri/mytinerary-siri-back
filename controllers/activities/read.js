a60a6096e80962aa4b3e3a2aeb1d1cbc45f8bf67
hernan siris <masmusic_madryn@hotmail.comm>
2023-09-13 09:44:02 -0300

    de tooodooo

--- controllers/activities/read.js
+++ controllers/activities/read.js
@@ -1,18 +1,28 @@
-import Activity from '../../models/Activity.js'
+import Activity from "../../models/Activity.js";
 
-export default async (req, res) => {
-    try {
-      let allActivities = await Activity.find();
+export default async (req, res,next) => {
+
+try{
+let queries = {}
+if (req.query.itinerary_id) {
+  queries.itinerary_id = req.query.itinerary_id
+}
+
+
+
+let allActivities = await Activity
+.find(queries,'-__v -createdAt -updatedAt')      
+.populate({
+        path:"itinerary_id",
+        select: "_id name photo"
+      })
       return res.status(200).json({
         success: true,
-        message: "Activities found!",
-        response: allActivities,
-      });
-    } catch (error) {
-      return res.status(400).json({
-        succes: false,
-        message: "Activities not found",
-        response: null,
-      });
-    }
-  };
+        message: 'activities found',
+        response: allActivities
+    })
+
+} catch (error) {
+  next(error)
+}
+}
